317a0a9b7252772b017d08d5833863c044cbb0a0
Ritik Channa <ritik.channa@nagarro.com>
2020-05-16 23:07:56 +0530

    Added ffmpeg bin loader

---
+++ src/main/java/net/bramp/ffmpeg/FFmpegLoader.java
@@ -0,0 +1,80 @@
+package net.bramp.ffmpeg;
+
+import java.io.*;
+import java.net.URL;
+import java.util.Enumeration;
+import java.util.jar.JarEntry;
+import java.util.jar.JarFile;
+
+public class FFmpegLoader {
+
+  public static String basePath;
+  public static String FFMPEG_PATH = basePath + "/ffmpeg";
+  public static String FFPROBE_PATH = basePath + "/ffprobe";
+  public static String FFPLAY_PATH = basePath + "/ffplay";
+
+  static {
+    try {
+      basePath = extractFFMPEG();
+    } catch (Exception e) {
+      e.printStackTrace();
+      throw new RuntimeException("Unable to initialize FFMPEG " + e.getMessage());
+    }
+  }
+
+  private static String extractFFMPEG() throws IOException, InstantiationException {
+    String name = System.getProperty("os.name").trim().toLowerCase();
+    String arch = System.getProperty("os.arch").trim().toLowerCase();
+
+    if (name.contains("windows") && arch.contains("32")) {
+      return extractResource("win32");
+    } else if (name.contains("windows") && arch.contains("64")) {
+      return extractResource("win64");
+    } else if (name.contains("mac")) {
+      return extractResource("mac");
+    } else throw new InstantiationException(name + " : " + arch + " not supported.");
+  }
+
+  private static String extractResource(String os) throws IOException {
+    final File jarFile =
+        new File(FFmpegLoader.class.getProtectionDomain().getCodeSource().getLocation().getPath());
+    String path;
+    if (jarFile.isFile()) { // Run with JAR file
+      path = jarFile.getParent();
+      final JarFile jar = new JarFile(jarFile);
+      final Enumeration<JarEntry> entries = jar.entries(); //gives ALL entries in jar
+      while (entries.hasMoreElements()) {
+        final String name = entries.nextElement().getName();
+        if (name.startsWith("bin/" + os + "/")) { //filter according to the path
+          copyFile(name, path);
+        }
+      }
+      jar.close();
+    } else { // Run with IDE
+      final URL url = FFmpegLoader.class.getResource("/bin/" + os);
+      path = url.getPath();
+    }
+    return path;
+  }
+
+  private static boolean copyFile(String srcFile, String destFolder) throws IOException {
+    srcFile = srcFile.replace('\\', '/');
+    destFolder = destFolder.replace('\\', '/');
+    File directory = new File(destFolder);
+    if (!directory.exists()) directory.mkdir();
+    srcFile = "/" + srcFile.substring(srcFile.lastIndexOf("bin/"));
+    try (InputStream inputStream = FFmpegLoader.class.getResourceAsStream(srcFile);
+        OutputStream outputStream =
+            new FileOutputStream(
+                destFolder + "/" + srcFile.substring(srcFile.lastIndexOf('/') + 1)); ) {
+      int readBytes;
+      byte[] buffer = new byte[4096];
+      while ((readBytes = inputStream.read(buffer)) > 0) {
+        outputStream.write(buffer, 0, readBytes);
+      }
+    } catch (Exception e) {
+      return false;
+    }
+    return true;
+  }
+}
